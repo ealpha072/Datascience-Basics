7d31ede3069218b7318e73cb344c852887647124
ealpha072 <ealpha072@gmail.com>
2021-04-02 16:19:17 +0300

    Roullette simulation game

---
+++ script.py
@@ -0,0 +1,20 @@
+from random import randint
+randomNumber = randint(0, 49)
+playerNumber=32
+playAmount =550
+totalAmount=500
+
+if playerNumber > 49 or playerNumber < 0:
+    print("Invalid number")
+else:
+    if playAmount > totalAmount:
+        print("High stakes")
+    else:
+        if playerNumber == randomNumber:
+            playAmount *=50
+            totalAmount +=playAmount
+            print("You won!!!", playAmount, "total amount is", totalAmount)
+        else:
+            playAmount -=playAmount
+            totalAmount -=playAmount
+            print("You lost!!!")
