69a7c5dac0ff557f0fe5836339fff326596ddd4f
ealpha072 <ealpha072@gmail.com>
2021-04-16 13:27:34 +0300

    Meging, joining and slicing dataframes

--- df.py
+++ df.py
@@ -1,6 +1,22 @@
 import pandas as pd 
 
-myData = {'Day':[1,2,3,4,5,6], 'Visitors':[900,400,300,500,200,700],'Bounce Rate':[20,34,25,15,25,40]}
+myData1 = {'Day':[1,2,3,4,5,6], 'Visitors':[900,400,300,500,200,700],'Bounce Rate':[20,34,25,15,25,40]}
+myData2 = {'HPI':[50,50,60,70],'Int_r':[10,15,12,10]}
+myData3 = {'HPI':[50,50,60,70],'Int_r':[10,15,12,10]}
+index = [2001,2002,2003,2004]
+myDf = pd.DataFrame(myData1)
 
-myDf = pd.DataFrame(myData)
-print(myDf)
+df1 = pd.DataFrame(myData1)
+df2 = pd.DataFrame(myData2,index)
+df3 = pd.DataFrame(myData3,index)
+
+#slicing dataframes
+
+firstTwo = myDf.head(2)
+lastThree = myDf.tail(3)
+
+##Merging and joining 
+merged = pd.merge(df2,df3, on = 'HPI')
+
+print(merged)
+
